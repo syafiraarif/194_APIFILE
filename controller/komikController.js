0d4757602467f1e82c4caa7e290404b7a97daeb3
syairaarif <putrisyafira4444@gmail.com>
2025-11-25 14:49:30 +0700

    membuat fungsi supaya nanti kita update data komik

--- controller/komikController.js
+++ controller/komikController.js
@@ -43,3 +43,24 @@ async function getKomikById(req, res) {
     }
 }
 
+async function updateKomik(req, res) {
+    try {
+        const komikData = req.body;
+
+        if (req.file) {
+            komikData.imageType = req.file.mimetype;
+            komikData.imageName = req.file.originalname;
+            komikData.imageData = req.file.buffer;
+        }
+
+        const result = await komikService.updateKomik(db, req.params.id, komikData);
+        res.json({ success: true, data: result });
+    } catch (error) {
+        res.status(400).json({ success: false, error: error.message });
+    }
+}
+
+
+
+
+
