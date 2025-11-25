5ad1798e70069dc0bb976ced55710aabc777af94
syairaarif <putrisyafira4444@gmail.com>
2025-11-25 14:25:30 +0700

    membuat file komik.js untuk memberi tahu bahwa tabel komik di database perpustakaan itu ada 4

---
+++ models/komik.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define('Komik', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        judul: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        author: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        deskripsi: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+
+        imageType: DataTypes.STRING,
+        imageName: DataTypes.STRING,
+        imageData: DataTypes.BLOB('long'),
+    }, {
+        tableName: 'komik',
+    });
+
+    return Komik;
+};
